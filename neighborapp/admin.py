972ab336eb24f61909bb2e7b5978e7b895b9bc2f
joankirui <joankirui99@gmail.com>
2022-01-06 23:31:56 +0300

    add methods for Neighborhood model

--- neighborapp/admin.py
+++ neighborapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from neighborapp.models import Profile
+from neighborapp.models import Neighborhood, Profile
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Neighborhood)
--- neighborapp/models.py
+++ neighborapp/models.py
@@ -5,10 +5,23 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
-class Neighbourhood(models.Model):
+class Neighborhood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     admin = models.ForeignKey('Profile',on_delete=models.CASCADE, related_name='hood')
+
+    def __str__(self):
+        return f'{self.name}hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls,search_term):
+        return cls.objects.filter(title__icontains=search_term)
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
@@ -17,7 +30,7 @@ class Profile(models.Model):
     contact = models.EmailField(max_length=100)
     name = models.CharField(blank=True,max_length=100)
     location = models.CharField(blank=True,max_length=100)
-    neighbourhood = models.ForeignKey('NeighbourHood', on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+    neighborhood = models.ForeignKey('NeighborHood', on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
     def __str__(self):
         return f'{self.user.username}Profile'
