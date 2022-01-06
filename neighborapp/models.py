9fb8844b60200d406b1677f8aa9b88a236c8d168
joankirui <joankirui99@gmail.com>
2022-01-06 23:13:24 +0300

    add Neighbourhood model

--- neighborapp/models.py
+++ neighborapp/models.py
@@ -5,6 +5,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey('Profile',on_delete=models.CASCADE, related_name='hood')
+
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
     prof_pic = CloudinaryField('image')
@@ -12,7 +17,7 @@ class Profile(models.Model):
     contact = models.EmailField(max_length=100)
     name = models.CharField(blank=True,max_length=100)
     location = models.CharField(blank=True,max_length=100)
-    neighborhood = models.ForeignKey(User,on_delete=models.CASCADE,related_name='members')
+    neighbourhood = models.ForeignKey('NeighbourHood', on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
     def __str__(self):
         return f'{self.user.username}Profile'
