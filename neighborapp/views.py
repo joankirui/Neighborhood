58a0d9ba813e67add42f6f31b70a842885ec5635
joankirui <joankirui99@gmail.com>
2022-01-06 16:24:00 +0300

    add a register function to the views

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from .forms import RegisterForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get['email']
+            username = form.cleaned_data.get('username')
+
+            messages.success(request,f'Account for {username} created,you can now login')
+            return redirect('login')
+        else:
+            form = RegisterForm()
+        return render(request,'django_registration/registration_form.html', {'form': form})
