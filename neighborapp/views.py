660ae5067fefc2e6cae2be6df7b92d1529d68045
joankirui <joankirui99@gmail.com>
2022-01-07 13:12:09 +0300

    update the view to add the forms I just created

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.http import HttpResponseRedirect, request
-
+from .forms import UpdateProfileForm,UpdateUserForm,RegisterForm
 
 # Create your views here.
 def registration(request):
@@ -28,3 +28,23 @@ def index(request):
 @login_required
 def profile(request):
     return render(request, 'profile.html')
+
+@login_required(login_url='/accounts/login/')
+def edit_profile(request):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect('profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+    args = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+    return render(request, 'editprofile.html', args)
