f9f267d5925dae6f04c816387ce48077ab6e9a4f
joankirui <joankirui99@gmail.com>
2022-01-09 13:25:13 +0300

    add a function for searching business

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -125,3 +125,17 @@ def leave_hood(request, id):
     request.user.profile.neighborhood = None
     request.user.profile.save()
     return redirect('index')
+
+@login_required(login_url='/accounts/login/')
+def search_business(request):
+
+    if 'title' in request.GET and request.GET["title"]:
+        search_term = request.GET.get("title")
+        results = Business.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"results":results})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
